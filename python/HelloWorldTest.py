fa57582d9b3b1076d20855d741c3412df40f74e2
Mieko Furuhashi <moc.mieko@okeim.com>
2019-11-01 22:20:38 -0500

    Fix based on review
    * Add driver=self.driver
    * Fix a typo
    
    Not a part of review:
    * Remove setting the path to chromedriver.
    If you follow the instruction in Wiki, no need
    to set it. webdrivers must be in $PATH or %PATH%
    variable.

--- python/HelloWorldTest.py
+++ python/HelloWorldTest.py
@@ -1,17 +1,14 @@
-from selenium import webdriver
+import re
 import unittest
-from pathlib import Path
-import os
-from sys import platform as _platform
 
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import re
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-class HelloWorldTests (unittest.TestCase):
+class HelloWorldTests(unittest.TestCase):
     TEN_SECOND = 10
 
     RIGHT_NAME_INPUT_ID = "et_pb_contact_name_1"
@@ -22,52 +19,48 @@ class HelloWorldTests (unittest.TestCase):
     MESSAGE_TEXT_CSS = "div#et_pb_contact_form_1 div.et-pb-contact-message"
 
     def setUp(self):
-        # It assumes chrome driver is saved at <user home>/drivers/chromedriver
-        user_home = str(Path.home())
-        driver_path = r"drivers"
-        driver_name = r"chromedriver.exe" if _platform == "Windows" else r"chromedriver"
-        webdriver_path = os.path.join(user_home, driver_path, driver_name)
         chrome_options: Options = webdriver.ChromeOptions()
         prefs = {"profile.default_content_setting_values.notifications": 2}
         chrome_options.add_experimental_option("prefs", prefs)
-        self.driver = webdriver.Chrome(executable_path=webdriver_path, chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(options=chrome_options)
 
     def test_verify_captcha(self):
+        driver = self.driver
         # Go to the site
-        self.driver.get("https://www.ultimateqa.com/filling-out-forms/")
+        driver.get("https://www.ultimateqa.com/filling-out-forms/")
 
         # Find right input field
-        right_name_input_element = self.driver.find_element(By.ID, HelloWorldTests.RIGHT_NAME_INPUT_ID)
+        right_name_input_element = driver.find_element(By.ID, HelloWorldTests.RIGHT_NAME_INPUT_ID)
         # TODO change it to your own name
         # Enter your name to right input field
         right_name_input_element.send_keys("My Name")
 
         # find right message field
-        right_message_input_element = self.driver.find_element(By.ID, HelloWorldTests.RIGHT_MESSAGE_INPUT_ID)
+        right_message_input_element = driver.find_element(By.ID, HelloWorldTests.RIGHT_MESSAGE_INPUT_ID)
         # enter "Hello World!"
         right_message_input_element.send_keys("Hello World!")
 
         # find the equation text
-        equation_text = self.driver.find_element(By.CLASS_NAME, HelloWorldTests.EQUATION_TEXT_CLASS).text
+        equation_text = driver.find_element(By.CLASS_NAME, HelloWorldTests.EQUATION_TEXT_CLASS).text
         # extract numbers
         numbers = list(map(int, re.split(r"\s*\+\s*", equation_text)))
         total = sum(numbers)
 
         # find the answer field
-        answer_input_element = self.driver.find_element(By.CSS_SELECTOR, HelloWorldTests.RIGHT_ANSWER_INPUT_CSS)
+        answer_input_element = driver.find_element(By.CSS_SELECTOR, HelloWorldTests.RIGHT_ANSWER_INPUT_CSS)
         # enter the answer
         answer_input_element.send_keys(total)
         # find the submit button and click it
-        self.driver.find_element(By.CSS_SELECTOR, HelloWorldTests.RIGHT_SUBMIT_BUTTON_CSS).click()
+        driver.find_element(By.CSS_SELECTOR, HelloWorldTests.RIGHT_SUBMIT_BUTTON_CSS).click()
 
         # Wait for visibility of message
-        success_text = WebDriverWait(self.driver, HelloWorldTests.TEN_SECOND).until(
+        success_text = WebDriverWait(driver, HelloWorldTests.TEN_SECOND).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, HelloWorldTests.MESSAGE_TEXT_CSS))).text
 
         # Verify obtained text is "Success"
         self.assertEqual(success_text, "Success", "Message should be Success")
 
-    def earDown(self):
+    def tearDown(self):
         # terminate webdriver
         self.driver.quit()
 
